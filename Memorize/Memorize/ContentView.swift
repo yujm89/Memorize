7c92bc218ef4f849be7a5f2b6977589ca0855edb
yumsteroni <jgflores1029@gmail.com>
2024-10-09 22:05:46 -0400

    Add Toggleable isFaceUp

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         HStack{
-            CardView(isFaceUp: true)
+            CardView()
             CardView()
             CardView(isFaceUp: true)
-            CardView()
+            CardView(isFaceUp: true)
         }
         .padding()
         .foregroundColor(.accentColor)
@@ -22,20 +22,23 @@ struct ContentView: View {
 }
 
 struct CardView: View{
-    var isFaceUp: Bool = false
+    @State var isFaceUp = false
+    
     var body: some View{
         ZStack {
+            let base = RoundedRectangle(cornerRadius: 12)
             if isFaceUp{
-                RoundedRectangle(cornerRadius: 12)
-                    .foregroundColor(.white)
-                RoundedRectangle(cornerRadius: 12)
-                    .strokeBorder(lineWidth: 10)
+                base.fill(.white)
+                base.strokeBorder(lineWidth: 2)
                 Text("👻").font(.largeTitle)
             }
             else {
-                RoundedRectangle(cornerRadius: 12)
+                base.fill()
 
             }
+        }
+        .onTapGesture {
+            isFaceUp.toggle()
         }
     }
 }
