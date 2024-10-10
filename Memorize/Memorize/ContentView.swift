dfb2ebb0a5a5c220087f36cff972fc8f1aa6bc9d
yumsteroni <jgflores1029@gmail.com>
2024-10-09 22:25:35 -0400

    Use ForEach to Create Cards from an Array

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -8,12 +8,12 @@
 import SwiftUI
 
 struct ContentView: View {
+    let emojis: Array<String> = ["👻", "🙈", "💀", "😈"]
     var body: some View {
         HStack{
-            CardView()
-            CardView()
-            CardView(isFaceUp: true)
-            CardView(isFaceUp: true)
+            ForEach(emojis.indices, id: \.self){ index in
+                CardView(content: emojis[index])
+            }
         }
         .padding()
         .foregroundColor(.accentColor)
@@ -22,7 +22,9 @@ struct ContentView: View {
 }
 
 struct CardView: View{
-    @State var isFaceUp = false
+    let content: String
+    
+    @State var isFaceUp = true
     
     var body: some View{
         ZStack {
@@ -30,7 +32,7 @@ struct CardView: View{
             if isFaceUp{
                 base.fill(.white)
                 base.strokeBorder(lineWidth: 2)
-                Text("👻").font(.largeTitle)
+                Text(content).font(.largeTitle)
             }
             else {
                 base.fill()
